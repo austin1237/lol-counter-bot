032fab1dea81c3c675751e8060d1e91760d33391
Austin Davis <austindavis4@gmail.com>
2023-10-01 17:03:31 -0600

    remote state for terraform

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ The following variables need to be set on your local/ci system.
 Url of the deployed lol-counter-api 
 ### DISCORD_BOT_TOKEN
 token that grants bot access to your discord server.
+
+## Deployment
+Deployment currently uses [Terraform](https://www.terraform.io/) to set up AWS services.
+
+### Setting up remote state
+Terraform has a feature called [remote state](https://www.terraform.io/docs/state/remote.html) which ensures the state of your infrastructure to be in sync for mutiple team members as well as any CI system.
+
+This project **requires** this feature to be configured. To configure **USE THE FOLLOWING COMMAND ONCE PER TEAM**.
+
+```bash
+cd terraform/remote-state
+terraform init
+terraform apply
+```
---
+++ terraform/remote-state/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  backend "local" {
+
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.4"
+    }
+  }
+  required_version = "~> 1.5"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "lol-counter-bot-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "lol-counter-bot-state-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
